b62790b00498e92361d0bc72571ba0e1f29a3f62
Priya Sharma <priyasharma@Priyas-MacBook-Air.local>
2024-12-27 17:07:12 +0530

    quiz-app cognito+amplify

---
+++ lets-ask/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "lesask1cc18164": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
